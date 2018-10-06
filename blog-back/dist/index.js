f50fae8e8211fcf64a76038e5850e8085a616006
Victor Masson <victor.massonv@gmail.com>
2018-10-06 14:41:32 +0200

    Add mongo db connection on raspberry

--- blog-back/dist/index.js
+++ blog-back/dist/index.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./server");
-const server = new server_1.default(4000, 'mongodb://localhost:27017/blog');
+// mongodb connection
+let mongoConnectionLaptop = 'mongodb://localhost:27017/blog'; // My laptop connection
+let mongoConnectionRaspberry = 'mongodb://192.168.1.37:27017/blog'; // My raspberry connection
+const server = new server_1.default(4000, mongoConnectionLaptop);
 server.start();
