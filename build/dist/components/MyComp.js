53c3428858679219318223bf1053fc603d27f115
Victor Masson <victor.massonv@gmail.com>
2018-10-07 18:17:12 +0200

    Remove build dir

--- build/dist/components/MyComp.js
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-var Mycomp = function (props) {
-    return (React.createElement("div", null,
-        React.createElement("h1", null, props.name)));
-};
-export default Mycomp;
-//# sourceMappingURL=MyComp.js.map
